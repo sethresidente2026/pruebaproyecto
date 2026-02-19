ad43a03153a19d3bd5f73250363ceaf234e22ce0
Sethxy123 <sethneftali12@gmail.com>
2026-02-19 12:53:54 -0600

    reportes terminados ahora PDF

--- app/Exports/HorariosExport.php
+++ app/Exports/HorariosExport.php
@@ -4,8 +4,13 @@ namespace App\Exports;
 
 use App\Models\Horario;
 use Maatwebsite\Excel\Concerns\FromCollection;
-
-class HorariosExport implements FromCollection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+class HorariosExport implements FromCollection,WithHeadings, WithMapping, ShouldAutoSize, WithTitle, WithStyles
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -16,15 +21,17 @@ class HorariosExport implements FromCollection
     }
     public function headings(): array
     {
-        return[
-            'Dia',
-            'Hora Inicio',
-            'Hora Fin',
-            'Grupo (Actividad)',
-            'Docente',
-            'Espacio/Salón',
+        return [
+            ['AGENDA OFICIAL DE HORARIOS - UGM RECTORÍA CENTRO'],
+            [''],
+            ['Día', 'Hora Inicio', 'Hora Fin', 'Grupo (Actividad)', 'Docente', 'Espacio/Salón']
         ];
     }
+    public function title(): string
+    {
+        return 'Agenda de Horarios';
+    }
+
     public function map($horario): array
     {
         return[
@@ -36,4 +43,15 @@ class HorariosExport implements FromCollection
             $horario->espacio->nombre ?? 'Sin asignar'
         ];
             }
+
+            public function styles(Worksheet $sheet)
+    {
+        // Combinamos de la A a la F para el título principal
+        $sheet->mergeCells('A1:F1');
+        
+        return [
+            1 => ['font' => ['bold' => true, 'size' => 14], 'alignment' => ['horizontal' => 'center']],
+            3 => ['font' => ['bold' => true]],
+        ];
+    }
 }
