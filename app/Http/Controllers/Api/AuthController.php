a47fc627c0f73cf11818007d17270b70196bde7c
sethresidente2026 <seth.residente@ugm.mx>
2026-02-26 11:28:34 -0600

    pruebas de seguridad

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; // <-- Asegúrate de que esta línea esté presente
-
+use App\Http\Resources\UserResource;
 class AuthController extends Controller
 {
     // app/Http/Controllers/Api/AuthController.php
@@ -13,24 +13,24 @@ class AuthController extends Controller
 // app/Http/Controllers/Api/AuthController.php
 
 public function login(Request $request) 
-{
-    $credenciales = $request->validate([
-        'email' => 'required|email',
-        'password' => 'required'
-    ]);
+    {
+        $credenciales = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
-    if (Auth::attempt($credenciales)) {
-        // Esta línea ya no fallará si activaste statefulApi() arriba
-        $request->session()->regenerate(); 
+        if (Auth::attempt($credenciales)) {
+            $request->session()->regenerate(); 
 
-        return response()->json([
-            'mensaje' => 'Bienvenido',
-            'usuario' => Auth::user()
-        ], 200);
+            return response()->json([
+                'mensaje' => 'Bienvenido',
+                // Usamos el recurso para filtrar Auth::user()
+                'usuario' => new UserResource(Auth::user())
+            ], 200);
+        }
+
+        return response()->json(['mensaje' => 'Credenciales inválidas'], 401);
     }
-
-    return response()->json(['mensaje' => 'Credenciales inválidas'], 401);
-}
 
     public function logout(Request $request)
     {
